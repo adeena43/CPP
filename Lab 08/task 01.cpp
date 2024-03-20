b3425c0c2a485bac328242a2bdb02e2123724923
adeena43 <142868138+adeena43@users.noreply.github.com>
2024-03-20 08:28:34 -0800

    Create task 01.cpp

---
+++ Lab 08/task 01.cpp
@@ -0,0 +1,48 @@
+/*
+Name->Adina Faraz
+Student ID->23k-0008
+*/
+#include<iostream>
+using namespace std;
+
+class Book{
+	protected:
+		string title;
+		string author;
+		string publisher;
+	public:
+		Book(string title, string author, string publisher):title(title),author(author),publisher(publisher)
+		{
+		}
+		
+		void display()
+		{
+			cout<<"Title: "<<title<<endl;
+			cout<<"Author: "<<author<<endl;
+			cout<<"Publisher: "<<publisher<<endl;		}
+};
+
+class FictionBook:public Book{
+	private:
+		string genre;
+		string protagonist;
+	
+	public:
+		FictionBook(string ttl, string atr, string pbl, string genre, string protagonist):Book(ttl, atr, pbl), genre(genre), protagonist(protagonist)
+		{}
+		
+		void display()
+		{
+			cout<<"Title: "<<title<<endl;
+			cout<<"Author: "<<author<<endl;
+			cout<<"Publisher: "<<publisher<<endl;
+			cout<<"Genre: "<<genre<<endl;
+			cout<<"Protagonist: "<<protagonist<<endl;	
+		}
+		
+};
+int main()
+{
+	FictionBook f1("Animal Farm"," George Orwell","Jane smith","Fiction","Markus Zusac");
+	f1.display();
+}
